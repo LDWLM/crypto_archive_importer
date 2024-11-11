8bb5e41f41d16c82ef0d1260d5a469ba519c3804
lidawei <lidawei@drojian.dev>
2024-11-11 10:44:56 +0800

    移动动态库模块到外部

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pdf2docx, os
+import pdf2docx, os, sys
+
+sys.path.append('/home/atlas/Projects/Python/archive/.venv/lib/python3.8/site-packages/pymupdf.zip')
 
 PDF = "/home/atlas/Downloads/test/bug/3-字体变黑黑框.pdf"
 OUTPUT = "output.docx"
@@ -6,7 +8,7 @@ OUTPUT = "output.docx"
 cv = pdf2docx.Converter(PDF)
 try:
     cv.convert(OUTPUT)
-    # os.remove(OUTPUT)
+    os.remove(OUTPUT)
     print(f"done")
 except Exception as e:
     pass
