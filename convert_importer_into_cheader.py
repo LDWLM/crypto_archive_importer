b2d30f8a982a8b46163c26ca52671116226f1d6e
lidawei <lidawei@drojian.dev>
2024-11-15 09:57:08 +0800

    code converter

---
+++ convert_importer_into_cheader.py
@@ -0,0 +1,24 @@
+IMPORTER = 'site-packages/secretzipimport.py'
+CHEADER = '/home/atlas/Projects/C/libpython38/importer.h'
+
+with open(IMPORTER, 'r') as sfile, open(CHEADER, 'w') as dfile:
+    dfile.write(f'#ifndef CONVERT_IMPORTER_INTO_CHEADER\n')
+    dfile.write(f'#define CONVERT_IMPORTER_INTO_CHEADER\n')
+    dfile.write('\n')
+    dfile.write('const char IMPORTER[] = {\n')
+    for line in sfile:
+        dfile.write('    ')
+        for char in line:
+            if char == '\n':
+                dfile.write("'\\n',")
+            elif char == '\\':
+                dfile.write("'\\\\',")
+            elif char == '\'':
+                dfile.write("'\\'',")
+            else:
+                dfile.write(f"'{char}',")
+        dfile.write('\n')
+    dfile.write('    0,\n')
+    dfile.write('};\n')
+    dfile.write('\n')
+    dfile.write(f'#endif /* CONVERT_IMPORTER_INTO_CHEADER */\n')
